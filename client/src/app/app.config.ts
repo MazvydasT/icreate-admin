15a7899d9777e66efb57334eb750ff9b003c6212
Maz T <mazvydas.tadaravicius@gmail.com>
2025-06-23 09:36:48 +0100

    API change

--- client/src/app/app.config.ts
+++ client/src/app/app.config.ts
@@ -1,6 +1,6 @@
 import {
   ApplicationConfig,
-  provideExperimentalZonelessChangeDetection
+  provideZonelessChangeDetection
 } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
@@ -10,7 +10,7 @@ import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideExperimentalZonelessChangeDetection(),
+    provideZonelessChangeDetection(),
     provideRouter(routes),
     provideAnimationsAsync(),
     provideHttpClient(),
